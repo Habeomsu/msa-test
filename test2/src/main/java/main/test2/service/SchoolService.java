cddd8a8eedf97a6843d62ae180bf6977f303fbcc
Habeomsu <hbs5186@naver.com>
2025-03-10 19:35:56 +0900

    school id로 가지고오기 구현

--- test2/src/main/java/main/test2/service/SchoolService.java
+++ test2/src/main/java/main/test2/service/SchoolService.java
@@ -1,9 +1,11 @@
 package main.test2.service;
 
 import main.test2.dto.SchoolRequestDto;
+import main.test2.dto.SchoolResponseDto;
 
 public interface SchoolService {
 
     public void save(SchoolRequestDto.School schoolDto);
+    public SchoolResponseDto.SchoolDto findById(Long id);
 
 }
