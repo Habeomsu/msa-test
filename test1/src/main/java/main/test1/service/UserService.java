3511b047ded2f3bbf051ddb916404fbf2c490110
Habeomsu <hbs5186@naver.com>
2025-03-11 21:42:12 +0900

    회원가입 구현 완료

--- test1/src/main/java/main/test1/service/UserService.java
+++ test1/src/main/java/main/test1/service/UserService.java
@@ -1,4 +1,11 @@
 package main.test1.service;
 
+import main.test1.dto.UserRequestDto;
+
 public interface UserService {
+
+    void join(UserRequestDto.JoinDto joinDto);
+
+
+
 }
--- test1/src/main/java/main/test1/service/UserServiceImpl.java
+++ test1/src/main/java/main/test1/service/UserServiceImpl.java
@@ -1,8 +1,44 @@
 package main.test1.service;
 
+import main.test1.dto.UserRequestDto;
+import main.test1.entity.Role;
+import main.test1.repository.UserRepository;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+import main.test1.entity.User;
 
 @Service
 public class UserServiceImpl implements UserService {
+
+    private final UserRepository userRepository;
+    private final BCryptPasswordEncoder bCryptPasswordEncoder;
+
+    public UserServiceImpl(UserRepository userRepository, BCryptPasswordEncoder bCryptPasswordEncoder) {
+        this.userRepository = userRepository;
+        this.bCryptPasswordEncoder = bCryptPasswordEncoder;
+    }
+
+    public void join(UserRequestDto.JoinDto joinDto){
+
+        String username = joinDto.getUsername();
+        String password = joinDto.getPassword();
+        String email = joinDto.getEmail();
+
+        boolean isexist = userRepository.existsByUsername(username);
+        if (isexist) {
+            throw new IllegalArgumentException("Username already exists");
+        }
+
+        User new_user = User.builder()
+                .username(username)
+                .password(bCryptPasswordEncoder.encode(password))
+                .email(email)
+                .role(Role.ROLE_USER)
+                .build();
+
+        userRepository.save(new_user);
+
+    }
 
 }
