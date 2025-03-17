ec02e234b22d5481f6afbc6e0ab1a0e7daa0c2a3
Habeomsu <hbs5186@naver.com>
2025-03-17 15:29:11 +0900

    학교 정보 레디스 저장 및 불러오기 구현

--- test1/src/main/java/main/test1/service/StudentServiceImpl.java
+++ test1/src/main/java/main/test1/service/StudentServiceImpl.java
@@ -1,5 +1,7 @@
 package main.test1.service;
 
+
+import lombok.extern.slf4j.Slf4j;
 import main.test1.client.SchoolFeignClient;
 import main.test1.dto.SchoolResponseDto;
 import main.test1.dto.StudentRequestDto;
@@ -7,17 +9,22 @@ import main.test1.dto.StudentResponseDto;
 import main.test1.entity.Student;
 import main.test1.global.exception.GeneralException;
 import main.test1.repository.StudentRepository;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Service;
 
 @Service
+@Slf4j
 public class StudentServiceImpl implements StudentService {
 
     private final StudentRepository studentRepository;
     private final SchoolFeignClient schoolFeignClient;
-
-    public StudentServiceImpl(StudentRepository studentRepository, SchoolFeignClient schoolFeignClient) {
+    private final RedisTemplate<String, SchoolResponseDto.School> redisTemplate;
+    public StudentServiceImpl(StudentRepository studentRepository, SchoolFeignClient schoolFeignClient,
+                              @Qualifier("schoolRedisTemplate") RedisTemplate<String, SchoolResponseDto.School> redisTemplate) {
         this.studentRepository = studentRepository;
         this.schoolFeignClient = schoolFeignClient;
+        this.redisTemplate = redisTemplate;
     }
 
     @Override
@@ -37,7 +44,16 @@ public class StudentServiceImpl implements StudentService {
         Student student = studentRepository.findById(id)
                 .orElseThrow(() -> new GeneralException("_STUDENT_NOT_FOUND"));
         Long schoolId = student.getSchool_id();
-        SchoolResponseDto.School school = schoolFeignClient.getSchool(schoolId);
+        String cacheKey = "school:" + schoolId;
+        SchoolResponseDto.School school = redisTemplate.opsForValue().get(cacheKey);
+        if (school == null) {
+            // 캐시에 없으면 Feign Client를 통해 학교 서비스 호출 후 캐싱
+            log.info("School not in cache");
+            school = schoolFeignClient.getSchool(schoolId);
+            redisTemplate.opsForValue().set(cacheKey, school);
+            log.info("School inserted");
+        }
+        log.info("School in cache");
         StudentResponseDto.StudentDto studentResponseDto = StudentResponseDto.StudentDto.builder()
                 .id(student.getId())
                 .name(student.getName())
